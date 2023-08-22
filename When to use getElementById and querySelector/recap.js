b0a78c2c2d9e2fd226dcaa80aad0da4740b5d5cc
sami5671 <samialam5671@gmail.com>
2023-08-22 19:34:46 +0600

    update

---
+++ When to use getElementById and querySelector/recap.js
@@ -0,0 +1,28 @@
+/**
+ * 1. how to use js with html and css
+ * 2. get element or elements from DOM
+ *
+ *
+ * 2.1: getElementById
+ * 2.2: getElementByClassName
+ * 2.3: querySelector
+ * 2.4: querySelectorAll
+ *
+ * 3. dynamically add element to the DOM: document.createElement
+ * 3.5: set value to the innerHTML
+ *
+ * 4. get value from a DOM element
+ * 4.1: use innerText (h1, p, h5, div, span, strong)
+ * 4.2: use value (input, textArea)
+ *
+ * 5. add event listener (click handler)
+ * 5.1:  click, focus, blur, onchange, keyup
+ * 5.2: addEventListener onClick = "myHandler()"
+ *
+ * 6. use function to reduce duplicate code
+ *
+ * 7. Event bubble and event delegate
+ */
+
+document.querySelectorAll(".user-container .disable h3");
+document.querySelectorAll(".my-user li");
